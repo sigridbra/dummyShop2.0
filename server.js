bfc3c9b21f6ea195e6e362e111a2b9aec3164ad3
Xillians <chkxillians@gmail.com>
2021-08-31 21:05:28 +0200

    woot?

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path')
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express()
-// All urls goto to index.html in /public folder [build folder]
-app.use("/", serveStatic(path.join(__dirname, '/public')))
+// All urls goto to index.html in /dist folder [build folder]
+app.use("/", serveStatic(path.join(__dirname, '/dist')))
 app.get(/.*/, function (req, res) {
-res.sendFile(path.join(__dirname, '/public/index.html'))
+res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 const port = process.env.PORT || 8081;
 app.listen(port)
