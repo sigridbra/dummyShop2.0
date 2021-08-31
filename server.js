5a3efff98fb3673215b0cd0690a259433c12b622
Xillians <chkxillians@gmail.com>
2021-08-31 20:57:07 +0200

    another attempt

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'public')));
-const port = process.env.PORT || 3000;
-app.listen(port);
+// Server file
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express()
+// All urls goto to index.html in /public folder [build folder]
+app.use("/", serveStatic(path.join(__dirname, '/public')))
+app.get(/.*/, function (req, res) {
+res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+const port = process.env.PORT || 8081;
+app.listen(port)
+console.log(`App is listening on port: ${port}`)
