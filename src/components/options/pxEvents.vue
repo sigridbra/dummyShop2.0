3a36ea8a89bf17073e835993e9cb6f3d4ec4be6f
Xillians <chkxillians@gmail.com>
2021-10-22 11:22:01 +0200

    rename

---
+++ src/components/options/pxEvents.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-for="(value, key, index) in events" :key="index">
+    <input type="checkbox" :id="key" v-model="events[key]" />
+    <label :for="key">{{ key }}</label>
+  </div>
+  <button/>
+</template>
+
+
+<script>
+export default {
+  name: "pxEvents",
+  data() {
+    return {
+      events: {
+         "onPaymentCompleted": false ,
+         "onPaymentPaid": false ,
+         "onEventNotification": false ,
+         "onInstrumentSelected": false ,
+         "onExternalRedirect": false ,
+         "onError": false ,
+         "onTermsOfServiceRequested": false ,
+         "onOutOfViewRedirect": false ,
+         "OnApllicationConfigured": false ,
+         "onPaymentAborted": false ,
+         "onPaymentFailed": false ,
+         "ExternalOpen": false ,
+         "onOutOfViewOpen": false 
+      },
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
